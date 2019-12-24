bd34c618b649dba118747748f420e198ee34ef7e
OnikenX <superjmm123@gmail.com>
2019-12-24 04:58:16 +0000

    first commit

---
+++ restartshell.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+kquitapp5 plasmashell 2> /dev/null && kstart5 plasmashell 2> /dev/null
---
+++ save.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo cp ./mine/* /usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/osd
